634f74b8fab6c09b415c8839d4052b14082bf922
Renan Souza <falecomrenansouza@gmail.com>
2017-08-02 20:13:03 -0300

    Arquivos base criados

---
+++ Slidex.class.php
@@ -0,0 +1,8 @@
+<?php 
+
+/*
+	Plugin Name: Podlist
+	Description: Shows the latest episodes of your podcast
+	Author: Renan Souza
+	Version: 0.1
+*/ 
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+
+/*
+	Plugin Name: Slidex
+	Description: Shows the latest episodes of your podcast
+	Author: Renan Souza
+	Version: 0.1
+*/ 
